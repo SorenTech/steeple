0fedbd4583db9c6142a99447138ee8eeac7619ee
Alexander Marshall <apmarshall@sorenmediagroup.com>
2014-11-28 21:37:59 -0500

    Added front-page template file (outline only, still needs actual content).

---
+++ front-page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+    <div id="inner-content" class="wrap cf">
+    
+        // Slider
+        
+        // Featured Widget
+        
+        // Text Content + Front-Page Sidebar
+        
+        // Front-Page Widget Area 2
+    
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
