70e712e75520710847d6ff3893836c637a464552
Alexander Marshall <apmarshall@sorenmediagroup.com>
2014-12-16 22:03:00 -0500

    Outlined Front-Page Divisions, Added Slider Import Code.

--- front-page.php
+++ front-page.php
@@ -4,13 +4,28 @@
 
     <div id="inner-content" class="wrap cf">
     
-        // Slider
+        <div id="front-page-slider" class="wrap cf"> 
+            //imports a slider from Metaslider plugin
+            <?php if(!exists(do_shortcode("[metaslider id=123]")) {
+                echo do_shortcode("metaslider id=123")); //replace 123 with id for front-page slider
+}
+                     ?>
+        </div>
         
-        // Featured Widget
+       <div id="front-page-featured-content" class="featured-content"> 
+           //highlights featured content (sticky posts) for front page
+            
+        </div>
         
-        // Text Content + Front-Page Sidebar
+        <div id="front-page-content">
+            //includes regular page content for front-page and front-page sidebar
         
-        // Front-Page Widget Area 2
+        </div>
+        
+       <div id="front-page-widget-area">
+           //includes a second specialty widget area for the front-page
+        
+        </div>
     
     </div>
 
