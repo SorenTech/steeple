5fa5cf0e8b405f7bda096201c2d668cc0c87b6bd
Alexander Marshall <apmarshall@sorenmediagroup.com>
2014-12-16 22:28:36 -0500

    Added basic featured area code.

--- front-page.php
+++ front-page.php
@@ -14,7 +14,10 @@
         
        <div id="front-page-featured-content" class="featured-content"> 
            //highlights featured content (sticky posts) for front page
-            
+            <?php if(!exists(post_type("featured")) {
+                         if (have_posts()) : while (have_posts()) : the_post();
+                         
+                         <?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
         </div>
         
         <div id="front-page-content">
