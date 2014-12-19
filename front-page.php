eb7efd8b597170f1fcb9d974c07158f0fa86f6a6
Alexander Marshall <apmarshall@sorenmediagroup.com>
2014-12-18 20:19:58 -0500

    Fixed improperly formatted comments.

--- front-page.php
+++ front-page.php
@@ -4,15 +4,12 @@
 
     <div id="inner-content" class="wrap cf">
     
-/* 
-        // Uncomment this section to active slider.
-        // <div id="front-page-slider" class="wrap cf"> 
-        //  <?php echo do_shortcode("metaslider id=123"); //replace 123 with id for front-page slider
+        <div id="front-page-slider" class="wrap cf"> 
+          <?php echo do_shortcode("metaslider id=123"); //replace 123 with id for front-page slider
                      ?>
-       // </div>
-*/
+    </div>
         
-       <div id="front-page-featured-content" class="featured-content"> //highlights featured content (sticky posts) for front page
+       <div id="front-page-featured-content" class="featured-content"> <!-- highlights featured content (sticky posts) for front page -->
             <?php // WP_Query - featured content
                      $args = array(
                          'posts_per_page' => '3',
@@ -31,7 +28,7 @@
                      wp_reset_postdata(); ?>
         </div>
         
-        <div id="front-page-content"> //includes regular page content for front-page and front-page sidebar
+        <div id="front-page-content"> <!-- includes regular page content for front-page and front-page sidebar -->
           <div id="front-page-article">  
                 <?php // WP_Query - front-page content
                      $args = array(
